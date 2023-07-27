9fc3d985c222c6054887f1553a40e9e243d42087
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2023-07-27 20:13:46 +0000

    chore(deps): bump wrangler to 3.4.0

---
+++ .changeset/sour-drinks-battle.md
@@ -0,0 +1,5 @@
+---
+'github-actions-cloudflare-pages': patch
+---
+
+chore(deps): bump wrangler to 3.4.0
--- dist/index.js
+++ dist/index.js
@@ -4632,7 +4632,7 @@ var createCloudflareDeployment = /* @__PURE__ */ __name(async () => {
     throw new Error(`${ERROR_KEY} branch is undefined`);
   }
   try {
-    const WRANGLER_VERSION = "3.3.0";
+    const WRANGLER_VERSION = "3.4.0";
     strict(WRANGLER_VERSION, "wrangler version should exist");
     await $`npx wrangler@${WRANGLER_VERSION} pages deploy ${directory} --project-name=${cloudflareProjectName} --branch=${branch} --commit-dirty=true --commit-hash=${commitHash}`;
     const deployment = await getCloudflareLatestDeployment();
