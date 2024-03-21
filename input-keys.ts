f3dc08821f44bd6f2e8d5f2e14244e2149941b71
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2024-03-21 20:41:16 +0000

    chore: fix lint errors

--- input-keys.ts
+++ input-keys.ts
@@ -1,10 +1,9 @@
-export const INPUT_KEY_CLOUDFLARE_ACCOUNT_ID = 'cloudflare-account-id' as const
-export const INPUT_KEY_CLOUDFLARE_API_TOKEN = 'cloudflare-api-token' as const
-export const INPUT_KEY_CLOUDFLARE_PROJECT_NAME =
-  'cloudflare-project-name' as const
-export const INPUT_KEY_DIRECTORY = 'directory' as const
-export const INPUT_KEY_GITHUB_ENVIRONMENT = 'github-environment' as const
-export const INPUT_KEY_GITHUB_TOKEN = 'github-token' as const
+export const INPUT_KEY_CLOUDFLARE_ACCOUNT_ID = 'cloudflare-account-id'
+export const INPUT_KEY_CLOUDFLARE_API_TOKEN = 'cloudflare-api-token'
+export const INPUT_KEY_CLOUDFLARE_PROJECT_NAME = 'cloudflare-project-name'
+export const INPUT_KEY_DIRECTORY = 'directory'
+export const INPUT_KEY_GITHUB_ENVIRONMENT = 'github-environment'
+export const INPUT_KEY_GITHUB_TOKEN = 'github-token'
 
 export const INPUT_KEYS_REQUIRED = [
   INPUT_KEY_CLOUDFLARE_ACCOUNT_ID,
