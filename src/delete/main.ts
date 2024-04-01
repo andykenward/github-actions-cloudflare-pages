875b0dcfed3860dbbe324f67f9b8766fe1c86a93
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2024-04-01 20:44:58 +0000

    feat(delete): batch delection with promise all

--- src/delete/main.ts
+++ src/delete/main.ts
@@ -1,4 +1,4 @@
-import {info, warning} from '@unlike/github-actions-core'
+import {debug, info, setFailed, warning} from '@unlike/github-actions-core'
 
 import {DeploymentStatusState} from '@/gql/graphql.js'
 
@@ -15,6 +15,85 @@ import {MutationCreateGitHubDeploymentStatus} from '@/common/github/deployment/s
 
 const PREFIX = `delete -`
 
+const batchDelete = async (
+  deployment: Awaited<ReturnType<typeof getGitHubDeployments>>[number]
+) => {
+  const payload = deployment.payload
+
+  try {
+    const {commentId, url, cloudflare} = getPayload(payload)
+
+    /**
+     * Delete Cloudflare deployment
+     */
+    const deletedCloudflareDeployment =
+      await deleteCloudflareDeployment(cloudflare)
+    if (!deletedCloudflareDeployment) return
+    /**
+     * On success of Cloudflare deployment delete GitHub deployment & comment.
+     */
+
+    const updateStatusGitHubDeployment = await request({
+      query: MutationCreateGitHubDeploymentStatus,
+      variables: {
+        environment: deployment.environment,
+        deploymentId: deployment.node_id,
+        environmentUrl: url,
+        logUrl: getCloudflareLogEndpoint(cloudflare),
+        state: DeploymentStatusState.Inactive
+      },
+      options: {
+        errorThrows: false
+      }
+    })
+
+    if (updateStatusGitHubDeployment.errors) {
+      warning(
+        `${PREFIX} Error updating GitHub deployment status: ${JSON.stringify(
+          updateStatusGitHubDeployment.errors
+        )}`
+      )
+      return
+    }
+
+    const deletedGitHubDeployment = commentId
+      ? await request({
+          query: MutationDeleteGitHubDeploymentAndComment,
+          variables: {
+            deploymentId: deployment.node_id,
+            commentId: commentId
+          },
+          options: {
+            errorThrows: false
+          }
+        })
+      : await request({
+          query: MutationDeleteGitHubDeployment,
+          variables: {
+            deploymentId: deployment.node_id
+          },
+          options: {
+            errorThrows: false
+          }
+        })
+
+    if (deletedGitHubDeployment.errors) {
+      warning(
+        `${PREFIX} Error deleting GitHub deployment: ${JSON.stringify(
+          deletedGitHubDeployment.errors
+        )}`
+      )
+    }
+    info(`${PREFIX} GitHub Deployment Deleted: ${deployment.node_id}`)
+  } catch (error) {
+    info(`${PREFIX} Deployment payload is not valid : ${JSON.stringify(error)}`)
+    return
+  }
+  /**
+   * Add comment with summary of deleted deployments etc?
+   */
+}
+
 export async function run() {
   const deployments = await getGitHubDeployments()
 
@@ -23,82 +102,12 @@ export async function run() {
     return
   }
 
-  for (const deployment of deployments) {
-    const payload = deployment.payload
-
-    try {
-      const {commentId, url, cloudflare} = getPayload(payload)
-
-      /**
-       * Delete Cloudflare deployment
-       */
-      const deletedCloudflareDeployment =
-        await deleteCloudflareDeployment(cloudflare)
-      if (!deletedCloudflareDeployment) continue
-      /**
-       * On success of Cloudflare deployment delete GitHub deployment & comment.
-       */
-
-      const updateStatusGitHubDeployment = await request({
-        query: MutationCreateGitHubDeploymentStatus,
-        variables: {
-          environment: deployment.environment,
-          deploymentId: deployment.node_id,
-          environmentUrl: url,
-          logUrl: getCloudflareLogEndpoint(cloudflare),
-          state: DeploymentStatusState.Inactive
-        },
-        options: {
-          errorThrows: false
-        }
-      })
-
-      if (updateStatusGitHubDeployment.errors) {
-        warning(
-          `${PREFIX} Error updating GitHub deployment status: ${JSON.stringify(
-            updateStatusGitHubDeployment.errors
-          )}`
-        )
-        continue
-      }
-
-      const deletedGitHubDeployment = commentId
-        ? await request({
-            query: MutationDeleteGitHubDeploymentAndComment,
-            variables: {
-              deploymentId: deployment.node_id,
-              commentId: commentId
-            },
-            options: {
-              errorThrows: false
-            }
-          })
-        : await request({
-            query: MutationDeleteGitHubDeployment,
-            variables: {
-              deploymentId: deployment.node_id
-            },
-            options: {
-              errorThrows: false
-            }
-          })
-
-      if (deletedGitHubDeployment.errors) {
-        warning(
-          `${PREFIX} Error deleting GitHub deployment: ${JSON.stringify(
-            deletedGitHubDeployment.errors
-          )}`
-        )
-      }
-      info(`${PREFIX} GitHub Deployment Deleted: ${deployment.node_id}`)
-    } catch (error) {
-      info(
-        `${PREFIX} Deployment payload is not valid : ${JSON.stringify(error)}`
-      )
-      continue
-    }
-    /**
-     * Add comment with summary of deleted deployments etc?
-     */
+  try {
+    const values = await Promise.all(
+      deployments.map(deployment => batchDelete(deployment))
+    )
+    debug(`${PREFIX} Deleted deployments: ${JSON.stringify(values)}`)
+  } catch (error) {
+    setFailed(`${PREFIX} Error deleting deployments: ${JSON.stringify(error)}`)
   }
 }
