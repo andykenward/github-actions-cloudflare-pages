8d8007d6285a7c1d646fe71ea41114bae82acb7b
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2024-04-18 20:58:36 +0000

    feat: delete output deletion to summary

--- src/delete/main.ts
+++ src/delete/main.ts
@@ -1,4 +1,4 @@
-import {debug, info, setFailed} from '@unlike/github-actions-core'
+import {debug, info, setFailed, summary} from '@unlike/github-actions-core'
 
 import {batchDelete} from '@/common/batch-delete.js'
 import {getGitHubDeployments} from '@/common/github/deployment/get.js'
@@ -10,6 +10,12 @@ export async function run() {
 
   if (deployments.length === 0) {
     info(`${PREFIX} No deployments found to delete`)
+
+    await summary
+      .addHeading('unlike-ltd/github-actions-cloudflare-pages')
+      .addBreak()
+      .addTable([['No deployments found to delete']])
+      .write()
     return
   }
 
@@ -18,6 +24,33 @@ export async function run() {
       deployments.map(deployment => batchDelete(deployment))
     )
     debug(`${PREFIX} Deleted deployments: ${JSON.stringify(values)}`)
+
+    if (values.length > 0) {
+      await summary
+        .addHeading('unlike-ltd/github-actions-cloudflare-pages')
+        .addBreak()
+        .addHeading('Deleted Deployments')
+        .addBreak()
+        .addTable([
+          [
+            {data: 'deploymentId', header: true},
+            {data: 'success', header: true},
+            {data: 'environment', header: true},
+            {data: 'environmentUrl', header: true},
+            {data: 'commentId', header: true},
+            {data: 'error', header: true}
+          ],
+          ...values.map(value => [
+            value.deploymentId,
+            value.success.toString(),
+            value.environment,
+            value.environmentUrl || '',
+            value.commentId || '',
+            value.error || ''
+          ])
+        ])
+        .write()
+    }
   } catch (error) {
     setFailed(`${PREFIX} Error deleting deployments: ${JSON.stringify(error)}`)
   }
