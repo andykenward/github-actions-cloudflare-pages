21b577d78080e66009bd21627fada671d7c30d86
Andy Kenward <andy@unlike.dev>
2024-06-29 10:23:34 +0000

    chore(deps-dev): bump @octokit/graphql-schema from 15.22.0 to 15.23.0

--- __generated__/gql/graphql.ts
+++ __generated__/gql/graphql.ts
@@ -17956,6 +17956,8 @@ export type PropertyTargetDefinition = {
   name: Scalars['String']['output'];
   /** The values to match for */
   propertyValues: Array<Scalars['String']['output']>;
+  /** The source of the property. Choose 'custom' or 'system'. Defaults to 'custom' if not specified */
+  source?: Maybe<Scalars['String']['output']>;
 };
 
 /** A property that must match */
@@ -17964,6 +17966,8 @@ export type PropertyTargetDefinitionInput = {
   name: Scalars['String']['input'];
   /** The values to match for */
   propertyValues: Array<Scalars['String']['input']>;
+  /** The source of the property. Choose 'custom' or 'system'. Defaults to 'custom' if not specified */
+  source?: InputMaybe<Scalars['String']['input']>;
 };
 
 /** A user's public key. */
