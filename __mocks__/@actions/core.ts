eabd1dfb9f06841e6b56ad31ca019e0d4ec8eb94
Andy Kenward <andy@unlike.dev>
2024-08-31 17:23:17 +0000

    chore: upgrade vitest from 1.6.0 to 2.0.5

--- __mocks__/@actions/core.ts
+++ __mocks__/@actions/core.ts
@@ -1,10 +1,9 @@
 import * as core from '@actions/core'
 import {vi} from 'vitest'
 
-export const getInput = vi.fn<
-  Parameters<typeof core.getInput>,
-  ReturnType<typeof core.getInput>
->((name: string, options) => core.getInput(name, options))
+export const getInput = vi.fn<typeof core.getInput>((name: string, options) =>
+  core.getInput(name, options)
+)
 
 export const setOutput = vi.fn()
 
